0a6040c6bda9bf08c971fc2d7ca1021343594021
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2026-04-07 02:10:13 -0500

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+
+        vector<vector<int>> adj(n);
+        for (const auto& e : edges)
+        {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        unordered_set<int> visited;
+        if (!dfs(0, -1, visited, adj))
+        {
+            return false;
+        }
+        return visited.size() == n;
+    }
+
+    bool dfs(int node, int parent, unordered_set<int>& visited, const vector<vector<int>>& adj)
+    {
+        if (visited.count(node))
+        {
+            return false;
+        }
+        visited.insert(node);
+        for (int edge : adj[node])
+        {
+            if (edge == parent)
+            {
+                continue;
+            }
+            if (!dfs(edge, node, visited, adj))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
