bbfcdb80376261fdc1d6c5404143d8a857fc5cf2
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2026-05-20 22:47:34 -0500

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    unordered_set<int> visiting;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj_map;
+        for (const auto& pre: prerequisites)
+        {
+            adj_map[pre[0]].push_back(pre[1]);
+        }
+        for (int i = 0; i < numCourses; i ++)
+        {
+            if (!dfs(i, prerequisites, adj_map))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool dfs(int course, vector<vector<int>>& prerequisites, unordered_map<int, vector<int>>& adj_map)
+    {
+        if (visiting.contains(course))
+        {
+            return false;
+        }
+        if (adj_map[course].empty())
+        {
+            return true;
+        }
+        visiting.insert(course);
+        for (int i : adj_map[course])
+        {
+            if(!dfs(i, prerequisites, adj_map))
+            {
+                return false;
+            }
+        }
+        visiting.erase(course);
+        adj_map[course].clear();
+        return true;
+    }
+};
