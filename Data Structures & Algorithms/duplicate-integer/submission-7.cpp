b1d21cdc4a36d6bb60b843d371457356bf720805
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2026-05-22 22:15:21 -0500

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for (int i : nums)
+        {
+            if (set.contains(i))
+            {
+                return true;
+            }
+            set.insert(i);
+        }
+        return false;
+    }
+};
