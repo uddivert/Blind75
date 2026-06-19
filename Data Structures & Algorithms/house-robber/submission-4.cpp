add938b31327536f0e26cfe886c46bd8614818ab
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2026-06-19 14:07:06 -0500

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> cache(nums.size(), -1);
+        return dfs(nums, cache, 0);
+    }
+    int dfs(vector<int>& nums, vector<int>& cache, int index)
+    {
+        if (index >= nums.size())
+        {
+            return 0;
+        }
+        if (cache[index] != -1)
+        {
+            return cache[index];
+        }
+        return cache[index] = max(dfs(nums, cache, index + 1), nums[index] + dfs(nums, cache, index + 2));
+    }
+};
