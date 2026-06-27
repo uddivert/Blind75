5bac41db171c5db21e4ad91044b9483308e71aa9
Uddhav Swami <71356488+uddivert@users.noreply.github.com>
2026-06-27 00:57:54 -0500

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n);
+        dp[0] = 1;
+        dp[1] = 2;
+        if (n < 2)
+        {
+            return n;
+        }
+        for(int i = 2; i < n; i ++)
+        {
+            dp[i] = dp [i -1] + dp[i-2];
+            
+        }
+        return dp[dp.size() -1 ];
+    }
+};
